42957596aa29ba3c6525a3cae534e29fa5268012
Cameron Brown <me@cbrxyz.com>
2024-11-17 15:23:12 -0500

    nvim: Enhance fzf-lua style and keymaps, add utils

--- nvim/lua/plugins/searching.lua
+++ nvim/lua/plugins/searching.lua
@@ -5,11 +5,54 @@ return {
         dependencies = { "nvim-tree/nvim-web-devicons" },
         config = function()
             -- calling `setup` is optional for customization
-            require("fzf-lua").setup({})
+            require("fzf-lua").setup({
+                winopts = {
+                    height = 0.4,
+                    width = 0.4,
+                    row = 0.5,
+                    hl = { normal = "Pmenu" },
+                    border = "none",
+                },
+                fzf_opts = {
+                    ["--no-info"] = "",
+                    ["--info"] = "hidden",
+                    ["--padding"] = "13%,5%,13%,5%",
+                    ["--header"] = " ",
+                    ["--no-scrollbar"] = "",
+                },
+                files = {
+                    formatter = "path.filename_first",
+                    git_icons = true,
+                    prompt = "files:",
+                    no_header = true,
+                    cwd_header = false,
+                    cwd_prompt = false,
+                    cwd = require("utils").git_root(),
+                    actions = {
+                        ["ctrl-d"] = function(...)
+                            fzf.actions.file_vsplit(...)
+                            vim.cmd("windo diffthis")
+                            local switch = vim.api.nvim_replace_termcodes("<C-w>h", true, false, true)
+                            vim.api.nvim_feedkeys(switch, "t", false)
+                        end,
+                    },
+                },
+            })
             -- <leader>zf to search files
-            vim.api.nvim_set_keymap("n", "<leader>zf", "<cmd>lua require('fzf-lua').files()<cr>", { noremap = true, silent = true })
-            -- <leader>zg to search git files
-            vim.api.nvim_set_keymap("n", "<leader>zg", "<cmd>lua require('fzf-lua').git_files()<cr>", { noremap = true, silent = true })
+            vim.api.nvim_set_keymap("n", "<leader>zf", "<cmd>lua require('fzf-lua').files()<cr>",
+                { noremap = true, silent = true })
+            -- <leader>zg to grep
+            vim.api.nvim_set_keymap("n", "<leader>zg", "<cmd>lua require('fzf-lua').live_grep()<cr>",
+                { noremap = true, silent = true })
+            -- <leader>zd to search through definitions
+            vim.api.nvim_set_keymap("n", "<leader>zd", "<cmd>lua require('fzf-lua').lsp_document_symbols()<cr>",
+                { noremap = true, silent = true })
+            -- <leader>ze to search through diagnostics (errors)
+            vim.api.nvim_set_keymap("n", "<leader>ze", "<cmd>lua require('fzf-lua').lsp_document_diagnostics()<cr>",
+                { noremap = true, silent = true })
+            -- <leader>zE to search through diagnostics (all)
+            vim.api.nvim_set_keymap("n", "<leader>zE", "<cmd>lua require('fzf-lua').lsp_workspace_diagnostics()<cr>",
+                { noremap = true, silent = true })
         end
     },
 }
---
+++ nvim/lua/utils.lua
@@ -0,0 +1,8 @@
+local M = {}
+
+M.git_root = function()
+	local git_path = vim.fn.finddir(".git", ".;")
+	return vim.fn.fnamemodify(git_path, ":h")
+end
+
+return M
