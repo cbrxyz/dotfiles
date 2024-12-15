796bd22359eded42a2c4c657acab74d7598cb3ef
Cameron Brown <me@cbrxyz.com>
2024-12-14 22:10:13 -0500

    lua/plugins/statusline.lua: Add wordcount to status line in md files

--- nvim/lua/plugins/statusline.lua
+++ nvim/lua/plugins/statusline.lua
@@ -2,6 +2,25 @@ return {
     'nvim-lualine/lualine.nvim',
     requires = { 'nvim-tree/nvim-web-devicons', opt = true },
     config = function()
-        require("lualine").setup()
+        local function wordcount()
+            return tostring(vim.fn.wordcount().words) .. ' words'
+        end
+
+        local function is_markdown()
+            return vim.bo.filetype == "markdown" or vim.bo.filetype == "asciidoc"
+        end
+
+        require("lualine").setup({
+            sections = {
+                lualine_a = { 'mode' },
+                lualine_b = { 'branch', 'diff', 'diagnostics' },
+                lualine_c = { 'filename' },
+                lualine_x = {
+                    { wordcount, cond = is_markdown },
+                    'encoding', 'fileformat', 'filetype' },
+                lualine_y = { 'progress' },
+                lualine_z = { 'location' }
+            },
+        })
     end,
 }
