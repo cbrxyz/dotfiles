47ba1a35cd9359d8d78a6a11578cc55281ab0178
Cameron Brown <me@cbrxyz.com>
2024-12-14 22:20:52 -0500

    lua/plugins/statusline.lua: Add count of lines and words in visual selection

--- nvim/lua/plugins/statusline.lua
+++ nvim/lua/plugins/statusline.lua
@@ -10,6 +10,18 @@ return {
             return vim.bo.filetype == "markdown" or vim.bo.filetype == "asciidoc"
         end
 
+        local function is_visual()
+            return vim.fn.mode():find("[Vv]") ~= nil
+        end
+
+        function selection_count()
+            local starts = vim.fn.line("v")
+            local ends = vim.fn.line(".")
+            local count = starts <= ends and ends - starts + 1 or starts - ends + 1
+            local wc = vim.fn.wordcount()
+            return count .. "L/" .. tostring(wc.visual_words) .. "W"
+        end
+
         require("lualine").setup({
             sections = {
                 lualine_a = { 'mode' },
@@ -19,7 +31,7 @@ return {
                     { wordcount, cond = is_markdown },
                     'encoding', 'fileformat', 'filetype' },
                 lualine_y = { 'progress' },
-                lualine_z = { 'location' }
+                lualine_z = { 'location', { selection_count, cond = is_visual } }
             },
         })
     end,
