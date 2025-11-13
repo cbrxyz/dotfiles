e47fd37f2279f291eb370224aa863a484b1985a4
Cameron Brown <me@cbrxyz.com>
2025-11-12 22:40:21 -0500

    rework lsp.lua

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -14,85 +14,72 @@ return {
 			"j-hui/fidget.nvim",
 		},
 		config = function()
-			local configs = require("lspconfig/configs")
-			local util = require("lspconfig/util")
+			vim.api.nvim_create_autocmd("LspAttach", {
+				callback = function(event)
+					local bufnr = event.buf
+					local client = vim.lsp.get_client_by_id(event.data.client_id)
+					if not client then
+						return
+					end
 
-			-- Enable LSP Status!
-			local lsp_status = require("lsp-status")
-			lsp_status.register_progress()
+					---@diagnostic disable-next-line need-check-nil
+					if client.server_capabilities.completionProvider then
+						vim.bo[bufnr].omnifunc = "v:lua.vim.lsp.omnifunc"
+						-- vim.bo[bufnr].omnifunc = "v:lua.MiniCompletion.completefunc_lsp"
+					end
+					---@diagnostic disable-next-line need-check-nil
+					if client.server_capabilities.definitionProvider then
+						vim.bo[bufnr].tagfunc = "v:lua.vim.lsp.tagfunc"
+					end
 
-			lsp_status.config({
-				indicator_errors = "",
-				indicator_warnings = "",
-				indicator_info = "",
-				indicator_hint = "",
-				indicator_ok = "Ok",
-			})
-
-			vim.diagnostic.config({
-				virtual_text = true,
-			})
-
-			-- Use an on_attach function to only map the following keys
-			-- after the language server attaches to the current buffer
-			local on_attach = function(client, bufnr)
-				local function buf_set_keymap(...)
-					vim.api.nvim_buf_set_keymap(bufnr, ...)
-				end
-				local function buf_set_option(...)
-					vim.api.nvim_buf_set_option(bufnr, ...)
-				end
-
-				-- Enable completion triggered by <c-x><c-o>
-				buf_set_option("omnifunc", "v:lua.vim.lsp.omnifunc")
-
-				-- Enable inlay hints if supported
-				if client.server_capabilities.inlayHintProvider then
-					vim.lsp.inlay_hint.enable(false)
-					buf_set_keymap(
+					-- Keymaps
+					local opts = { noremap = true, silent = true }
+					vim.keymap.set("n", "gD", "<cmd>lua vim.lsp.buf.declaration()<CR>", opts)
+					vim.keymap.set("n", "gd", "<cmd>lua vim.lsp.buf.definition()<CR>", opts)
+					vim.keymap.set("n", "K", "<cmd>lua vim.lsp.buf.hover()<CR>", opts)
+					vim.keymap.set("n", "gi", "<cmd>lua vim.lsp.buf.implementation()<CR>", opts)
+					vim.keymap.set("n", "<C-k>", "<cmd>lua vim.lsp.buf.signature_help()<CR>", opts)
+					vim.keymap.set("n", "<space>wa", "<cmd>lua vim.lsp.buf.add_workspace_folder()<CR>", opts)
+					vim.keymap.set("n", "<space>wr", "<cmd>lua vim.lsp.buf.remove_workspace_folder()<CR>", opts)
+					vim.keymap.set(
 						"n",
-						"<leader>lh",
-						"<cmd>lua vim.lsp.inlay_hint.enable(not vim.lsp.inlay_hint.is_enabled())<CR>",
-						{ noremap = true, silent = true }
+						"<space>wl",
+						"<cmd>lua print(vim.inspect(vim.lsp.buf.list_workspace_folders()))<CR>",
+						opts
 					)
-				end
+					vim.keymap.set("n", "<space>D", "<cmd>lua vim.lsp.buf.type_definition()<CR>", opts)
+					vim.keymap.set("n", "<space>rn", "<cmd>lua vim.lsp.buf.rename()<CR>", opts)
+					vim.keymap.set("n", "<space>ca", "<cmd>lua vim.lsp.buf.code_action()<CR>", opts)
+					vim.keymap.set("n", "gr", "<cmd>lua vim.lsp.buf.references()<CR>", opts)
+					vim.keymap.set("n", "<space>e", "<cmd>lua vim.diagnostic.open_float()<CR>", opts)
+					vim.keymap.set("n", "[d", "<cmd>lua vim.diagnostic.jump({count = -1, float = true})<CR>", opts)
+					vim.keymap.set("n", "]d", "<cmd>lua vim.diagnostic.jump({count = 1, float = true})<CR>", opts)
+					vim.keymap.set("n", "<space>q", "<cmd>lua vim.diagnostic.setloclist()<CR>", opts)
+					-- also print "formatted!"
+					vim.keymap.set(
+						"n",
+						"<space>ff",
+						"<cmd>lua vim.lsp.buf.format( { async = true } ); print('formatted!')<CR>",
+						opts
+					)
 
-				-- Mappings.
-				local opts = { noremap = true, silent = true }
+					-- Enable inlay hints if supported
+					if client.server_capabilities.inlayHintProvider then
+						vim.lsp.inlay_hint.enable(false)
+						vim.keymap.set(
+							"n",
+							"<leader>lh",
+							"<cmd>lua vim.lsp.inlay_hint.enable(not vim.lsp.inlay_hint.is_enabled())<CR>",
+							{ noremap = true, silent = true }
+						)
+					end
 
-				-- See `:help vim.lsp.*` for documentation on any of the below functions
-				buf_set_keymap("n", "gD", "<cmd>lua vim.lsp.buf.declaration()<CR>", opts)
-				buf_set_keymap("n", "gd", "<cmd>lua vim.lsp.buf.definition()<CR>", opts)
-				buf_set_keymap("n", "K", "<cmd>lua vim.lsp.buf.hover()<CR>", opts)
-				buf_set_keymap("n", "gi", "<cmd>lua vim.lsp.buf.implementation()<CR>", opts)
-				buf_set_keymap("n", "<C-k>", "<cmd>lua vim.lsp.buf.signature_help()<CR>", opts)
-				buf_set_keymap("n", "<space>wa", "<cmd>lua vim.lsp.buf.add_workspace_folder()<CR>", opts)
-				buf_set_keymap("n", "<space>wr", "<cmd>lua vim.lsp.buf.remove_workspace_folder()<CR>", opts)
-				buf_set_keymap(
-					"n",
-					"<space>wl",
-					"<cmd>lua print(vim.inspect(vim.lsp.buf.list_workspace_folders()))<CR>",
-					opts
-				)
-				buf_set_keymap("n", "<space>D", "<cmd>lua vim.lsp.buf.type_definition()<CR>", opts)
-				buf_set_keymap("n", "<space>rn", "<cmd>lua vim.lsp.buf.rename()<CR>", opts)
-				buf_set_keymap("n", "<space>ca", "<cmd>lua vim.lsp.buf.code_action()<CR>", opts)
-				buf_set_keymap("n", "gr", "<cmd>lua vim.lsp.buf.references()<CR>", opts)
-				buf_set_keymap("n", "<space>e", "<cmd>lua vim.diagnostic.open_float()<CR>", opts)
-				buf_set_keymap("n", "[d", "<cmd>lua vim.diagnostic.jump({count = -1, float = true})<CR>", opts)
-				buf_set_keymap("n", "]d", "<cmd>lua vim.diagnostic.jump({count = 1, float = true})<CR>", opts)
-				buf_set_keymap("n", "<space>q", "<cmd>lua vim.diagnostic.setloclist()<CR>", opts)
-				-- also print "formatted!"
-				buf_set_keymap(
-					"n",
-					"<space>ff",
-					"<cmd>lua vim.lsp.buf.format( { async = true } ); print('formatted!')<CR>",
-					opts
-				)
-
-				-- Add support for LSP Status
-				lsp_status.on_attach(client)
-			end
+					-- Enable virtual text
+					vim.diagnostic.config({
+						virtual_text = true,
+					})
+				end,
+			})
 
 			vim.lsp.enable("clangd")
 			vim.lsp.enable("yamlls")
@@ -106,9 +93,6 @@ return {
 			vim.lsp.enable("ltex")
 			vim.lsp.enable("bashls")
 		end,
-	},
-	{
-		"nvim-lua/lsp-status.nvim",
 	},
 	{
 		"m-pilia/vim-ccls",
